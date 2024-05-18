6d1c2a39a4022d3f6f1dbff79e1939bcf09542b6
Ch Ihtasham <cihtasham32@gmail.com>
2024-05-18 10:30:19 +0500

    class 3

---
+++ Class 3/script.js
@@ -0,0 +1,19 @@
+// Data types in js
+/*  string
+    number
+    bigint
+    boolean
+    undefine
+    symbol
+    null
+    object
+    */
+
+let string= "this is a string"
+let number = 123
+let bool = false
+// let name; undefine
+let name= null
+
+console.log(typeof (number))
+
