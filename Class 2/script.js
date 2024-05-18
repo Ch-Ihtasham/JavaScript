84c2ce611bfee1520c5cf1a3c6783fd811fe71a6
Ch Ihtasham <cihtasham32@gmail.com>
2024-05-18 10:08:49 +0500

    commit

---
+++ Class 2/script.js
@@ -0,0 +1,7 @@
+const accountId = 1234
+let accountName ="Ch Muhammad Ihtasham"
+var accountState ="runing"
+accountName="Zeeshan Ayaz Baig"
+console.log(accountId)
+
+console.table([accountId,accountName,accountState])
