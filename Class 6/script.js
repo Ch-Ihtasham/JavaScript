3e5a2b67921b208ed30b102fb5667e4a481e9250
Ch Ihtasham <cihtasham32@gmail.com>
2024-05-20 13:28:13 +0500

    number

---
+++ Class 6/script.js
@@ -0,0 +1,10 @@
+let num=123
+console.log(num)
+let num2 = new Number(120)
+console.log(num2)
+console.log(num2.toString())
+console.log(num.toFixed(1))
+let num3 = 123.256
+console.log(num3.toPrecision(4))
+let a= 1000000
+console.log(a.toLocaleString('en-IN'))
