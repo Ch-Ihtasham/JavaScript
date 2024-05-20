c76e78dfc24d655a775273f903a065794cfba176
Ch Ihtasham <cihtasham32@gmail.com>
2024-05-20 12:32:31 +0500

    string

---
+++ Class 5/script.js
@@ -0,0 +1,17 @@
+const string = "hello world"
+console.log(string)
+console.log(string.length)
+console.log(string.charAt(2))
+console.log(string.charCodeAt(2))
+console.log(string.slice(0,4))
+console.log(string.substring(0,4))
+console.log(string.substr(0,1))
+console.log(string.concat(" kia hal ha"))
+const trim= "     hello   "
+console.log(trim)
+console.log(trim.trim())
+const replace = "ch Ihtahsam"
+console.log(replace)
+console.log(replace.replace("ch","Muhammad"))
+console.log(replace.split(" "))
+
