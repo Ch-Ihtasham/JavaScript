34b12c4fa0ac313416f40d7b652aa59b051cb4f6
Ch Ihtasham <cihtasham32@gmail.com>
2024-06-28 02:13:21 +0500

    class 10

---
+++ Class 10/script.js
@@ -0,0 +1,36 @@
+const array = ["1", "2"]
+const newarry = ["6", "7"]
+// array.push(newarry);
+// console.log(array.concat(newarry))
+// const arr= [...array,...newarry] its called spread operator
+// console.log(arr)
+// const array1 = [1,2,3,4,[1,2,[4,5]]]
+// console.log(array1.flat(Infinity))
+// const str = "1234"
+// console.log(Array.from(str))
+// let sum = 0
+// console.log(str[0])
+// for (let i = 0; i < str.length; i++) {
+
+//     sum = parseInt(str[i]) + sum;
+// }
+// console.log(sum)
+// let no = "123";
+// let news = String(no);
+// console.log(Array.from((no),Number))    
+
+let no1 = 12345;
+// let sum = 0
+console.log( Array.from(String(no1), Number))
+// console.log(no[0])
+// for (let i = 0; i < news.length; i++) {
+//     sum = news[i] + sum;
+// }
+// console.log(sum);
+// let no =5;
+// let nno =no.toString(2)
+// let nno =String(no)
+// let ne=new Array(no).fill(2)
+// console.log(ne);
+
+
