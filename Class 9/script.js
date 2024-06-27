cb6c72541a3cfa8996d90b97ba4cee32fb885f2c
Ch Ihtasham <cihtasham32@gmail.com>
2024-06-27 22:56:39 +0500

    commit

--- Class 9/script.js
+++ Class 9/script.js
@@ -1,5 +1,5 @@
-const arr = [1, 2, 3, 4, 5]
-const arr1 = new Array(1, 2, 3, 4)
+// const arr = [1, 2, 3, 4, 5]
+// const arr1 = new Array(1, 2, 3, 4)
 // console.log(arr)
 // console.log(arr1)
 // arr.push(6)
@@ -12,17 +12,28 @@ const arr1 = new Array(1, 2, 3, 4)
 // console.log(arr)
 // console.log(arr.includes(9))
 // console.log(arr.indexOf(4))
-const newarr = arr.slice(1, 3)
-console.log(newarr)
-console.log(arr)
-const newarr2 = arr.splice(1, 3)
-console.log(newarr2)
-console.log(arr)
-const array = [
-    { name: "Ihtasham", rollno: 12 },
-    { name: "shami", rollno: 123 }
-]
-const value = array
-array.map((item, i) => {
-    console.log(item, i);
-});
+// const newarr = arr.slice(1, 3)
+// console.log(newarr)
+// console.log(arr)
+// const newarr2 = arr.splice(1, 3)
+// console.log(newarr2)
+// console.log(arr)
+// const array = [
+//     { name: "Ihtasham", rollno: 12 },
+//     { name: "shami", rollno: 123 }
+// ]
+// const value = array
+// array.map((item, i) => {
+//     console.log(item.name);
+// });
+const arr = [1, 2, 3, 4, 5];
+function filter() {
+    let newarr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            newarr.push(arr[i]);
+        }
+    }
+    return newarr;
+}
+console.log(filter())
