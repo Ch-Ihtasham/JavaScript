003a0506c0c4ed49948046464e3b6913a507d2b5
Ch Ihtasham <cihtasham32@gmail.com>
2024-06-29 01:15:16 +0500

    claass11

--- Class 11/script.js
+++ Class 11/script.js
@@ -16,12 +16,29 @@ const obj = {
 // console.log(obj[sym])
 obj.email = "chihtasham@gamil.com"
 // Object.freeze(obj)
-obj.name = "muhammad Ihtasham"
-obj.greating = function me () {
-    console.log(`hello user ${this.name}`)
+// obj.name = "muhammad Ihtasham"
+// obj.greating = function me () {
+//     console.log(`hello user ${this.name}`)
+// }
+// console.log(obj.greating())
+// console.log(obj)
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+const obj3 = Object.assign({},obj1 ,obj2)
+// console.log(obj3)
+// const obj4 = new Object() create object through constructor
+const obj4 ={
+    id:123,
+    name:"Zeeshan",
+    email:"zeeshan@gmail.com"
 }
-console.log(obj.greating())
-console.log(obj)
-
+console.log(Object.keys(obj4))
+console.log(Object.values(obj4))
+console.log(Object.entries(obj4))
+Object.freeze(obj4)
+// Object.seal(obj4)
+// delete obj4.email
+// obj4.email="ihtasham"
+// console.log(obj4)
 
 
