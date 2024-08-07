1ed2ca741478c8e223f867031ac0e0c90dfcc3bd
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-08-07 12:29:28 +0500

    comit

--- Class 19/script.js
+++ Class 19/script.js
@@ -2,11 +2,21 @@ const promiseOne = new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log("Async task is complete")
         resolve()
-    }, 5000)
+    }, 1000)
 })
 
 
 promiseOne.then(() => {
     console.log('promise comsumed')
+})
+console.log('hello world')
+
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('prmoise two')
+        resolve()
+    }, 2000);
+}).then(() => {
+    console.log("promise two consumed")
 })
 
