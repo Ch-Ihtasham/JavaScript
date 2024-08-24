6204c092ac32056c796e796363e3fa7861ab92b6
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-08-25 01:24:31 +0500

    commit

--- Class 4/script.js
+++ Class 4/script.js
@@ -7,3 +7,16 @@
 // let abc = "123abc"
 // let number = Number(abc)
 // console.log(number)
+
+let array = [1, 2, 4, 4, 5]
+for (let i = 0; i < array.length; i++) {
+    let no = array[i] 
+    for ( ; a < array.length; a++) {
+        if (no + array[a] == 8) {
+            console.log(no, array[a])
+            break;
+        }
+    }
+
+
+}
