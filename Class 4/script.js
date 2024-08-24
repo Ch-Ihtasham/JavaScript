d28d534f4c7838153aadfb5596a10e78e1cac375
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-08-24 23:55:16 +0500

    commit

--- Class 4/script.js
+++ Class 4/script.js
@@ -1,9 +1,9 @@
-let a = 123
-console.log(typeof a)
-let b= "123"
-let ab = Number(b)
-console.log(typeof ab)
-console.log(ab)
-let abc = "123abc"
-let number = Number(abc)
-console.log(number)
+// let a = 123
+// console.log(typeof a)
+// let b= "123"
+// let ab = Number(b)
+// console.log(typeof ab)
+// console.log(ab)
+// let abc = "123abc"
+// let number = Number(abc)
+// console.log(number)
